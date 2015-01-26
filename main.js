dba6582c3d43260086165089f1ac5a7a3cbb2677
Alon Dahari <climbinghobo@gmail.com>
2015-01-26 13:13:55 -0700

    add problem2

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 var animals = ['rat', 'cat', 'butterfly', 'marmot', 'ocelot'];
 
 var problem1 = function(arr){
-  arr.pop();
-  return arr;
+  return arr.slice(0, -1);
 };
 
+var problem2 = function(arr){
+  return arr.filter(function (val, i) {
+    return (i%2 === 0);
+  });
+};
+
+
+
 console.log('problem1:', problem1(animals));
+console.log('problem2:', problem2(animals));
+console.log('problem3:', problem3(animals));
+console.log('problem4:', problem4(animals));
